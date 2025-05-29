190b1c139a247cf405fefe86e62327ad0e3ee548
davidjung1001 <davidjung1001@gmail.com>
2025-05-28 23:09:30 -0500

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: 'https://soccer-directory.vercel.app', // your website URL here
+  generateRobotsTxt: true,            // (optional) generate robots.txt
+  // ...other options you want
+};
