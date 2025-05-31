9a65af462cfddb590824176f81f9da37b01a8d42
davidjung1001 <davidjung1001@gmail.com>
2025-05-31 17:51:17 -0500

    auth pages removed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: 'https://soccer-directory.vercel.app', // your website URL here
-  generateRobotsTxt: true,            // (optional) generate robots.txt
-  // ...other options you want
+  siteUrl: 'https://soccer-directory.vercel.app',
+  generateRobotsTxt: true,
+  exclude: ['/my-fields', '/edit-field/*', '/rental-field-manage'], // Private routes
 };
