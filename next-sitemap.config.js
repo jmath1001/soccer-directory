537aa79db2de8eb869890f371542864e28ffbb6b
davidjung1001 <davidjung1001@gmail.com>
2025-06-01 21:37:03 -0500

    next config removed

--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  siteUrl: 'https://soccer-directory.vercel.app',
-  generateRobotsTxt: true,
-  exclude: ['/my-fields', '/edit-field/*', '/rental-field-manage', '/add-field', '/login', '/logout', '/register','submit-field'], // Private routes
-};
