f2c43a57e04458e639b38cc96b421094610b05c7
davidjung1001 <davidjung1001@gmail.com>
2025-06-08 23:11:25 -0500

    small sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,5 @@ module.exports = {
   siteUrl: 'https://soccer-directory.vercel.app',
   generateRobotsTxt: true,
   outDir: './public',
+  exclude: ['/my-fields', '/rental-field-manage', '/add-field', '/add-league', '/login', '/submit-field', '/add-pickup-game', '/register'],
 }
